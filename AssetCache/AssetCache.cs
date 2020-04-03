08147de1a779c1506f6b893d653cc857d9abf673
Tairin <tairinjane@gmail.com>
2020-04-03 15:19:17 +0700

    Implemented assets merge

--- AssetCache/AssetCache.cs
+++ AssetCache/AssetCache.cs
@@ -1,20 +1,22 @@
-﻿﻿using System;
+﻿using System;
 using System.Collections.Generic;
- using System.IO;
- using System.Security.Cryptography;
- using System.Text;
- using System.Text.RegularExpressions;
- using YamlDotNet.Serialization;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using YamlDotNet.Serialization;
 
- namespace AssetCache {
-    public class AssetCache : IAssetCache{
-        private Cache cache;
+namespace AssetCache {
+    public class AssetCache : IAssetCache {
+        private Dictionary<ulong, SceneObject> cache;
         private string assetPath;
-        
+
         private readonly Regex stringFieldPattern = new Regex(@"(?<key>\w+): {(?<value>.+)}");
         private readonly Regex fileIDPattern = new Regex(@"- {fileID: (?<id>\d+)}");
         private const string componentKey = "m_Component";
         private const string childrenKey = "m_Children";
+
         public object Build(string path, Action interruptChecker) {
             var fileStream = File.OpenText(path);
             var cacheDict = new Dictionary<ulong, SceneObject>();
@@ -71,16 +73,16 @@ using System.Collections.Generic;
                 cacheDict.Add(key, new SceneObject(GetHash(hashString), objDict));
             }
 
-            return new Cache(cacheDict);
+            return cacheDict;
         }
-        
+
         private static string GetHash(string input) {
             var md5 = MD5.Create();
             var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
 
             return Convert.ToBase64String(hash);
         }
-        
+
         private static ulong GetObjectFileId(string line) {
             var pattern = new Regex(@"--- !u!\d+ &(?<id>\d+)");
             var match = pattern.Match(line);
@@ -89,7 +91,7 @@ using System.Collections.Generic;
                 return Convert.ToUInt64(key);
             }
 
-            throw new ArgumentException("No id in string: " + line);
+            throw new ArgumentException("No id in " + line);
         }
 
         private static Dictionary<string, object> ReadObject(string obj) {
@@ -100,19 +102,52 @@ using System.Collections.Generic;
         }
 
         public void Merge(string path, object result) {
-            cache = (Cache) result;
+            var resultDict = (Dictionary<ulong, SceneObject>) result;
+            if (path != assetPath) {
+                assetPath = path;
+                cache = resultDict;
+                return;
+            }
+
+            var updateKeys = cache.Keys.Intersect(resultDict.Keys);
+            var removeKeys = cache.Keys.Except(resultDict.Keys);
+            var addKeys = resultDict.Keys.Except(cache.Keys);
+
+            foreach (var key in removeKeys) {
+                cache.Remove(key);
+            }
+
+            foreach (var key in addKeys) {
+                cache.Add(key, resultDict[key]);
+            }
+
+            foreach (var key in updateKeys) {
+                if (cache[key].Hash != resultDict[key].Hash) {
+                    cache[key] = resultDict[key];
+                }
+            }
         }
 
         public int GetLocalAnchorUsages(ulong anchor) {
-            return cache.GetLocalAnchorUsages(anchor);
+            var count = 0;
+            foreach (var obj in cache.Values) {
+                count += obj.GetLocalAnchorUsages(anchor);
+            }
+
+            return count;
         }
 
         public int GetGuidUsages(string guid) {
-            return cache.GetGuidUsages(guid);
+            var count = 0;
+            foreach (var obj in cache.Values) {
+                count += obj.GetGuidUsages(guid);
+            }
+
+            return count;
         }
 
         public IEnumerable<ulong> GetComponentsFor(ulong gameObjectAnchor) {
-            return cache.GetComponentsFor(gameObjectAnchor);
+            return cache[gameObjectAnchor].GetComponents();
         }
     }
 }
